86553effcae7d034f052d18da07aa06bc8acb126
Nick Mathews <nick@codemettle.com>
2015-03-02 18:49:16 -0500

    take a picture and set to an ImageView

--- app/src/main/java/com/boboddy/vault/MainActivity.java
+++ app/src/main/java/com/boboddy/vault/MainActivity.java
@@ -1,17 +1,34 @@
 package com.boboddy.vault;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageView image;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mPhotoPath = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.image);
     }
 
 
@@ -32,8 +49,53 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.add_item) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(takePictureIntent.resolveActivity(getPackageManager())!=null)
+            {
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                }
+                catch(IOException ioex)
+                {
+//                    Log.e("GeoPhotos", "COULD NOT CREATE IMAGE FILE");
+//                    Log.e("GeoPhotos", ioex.getMessage());
+                }
+                if(photoFile!=null) {
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(mPhotoPath!= "")
+        {
+//            Log.d("CameraActivity", mPhotoPath);
+            Bitmap imageBitmap = BitmapFactory.decodeFile(mPhotoPath);
+            //Scale the Bitmap so that it fits on canvas.
+            if(imageBitmap != null)
+            {
+                Bitmap scaled = imageBitmap.createScaledBitmap(imageBitmap, (imageBitmap.getWidth()/4), (imageBitmap.getHeight()/4),false);
+                image.setImageBitmap(scaled);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile("image", ".jpg", storageDir);
+        mPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
