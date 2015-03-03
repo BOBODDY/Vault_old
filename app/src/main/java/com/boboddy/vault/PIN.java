6d8025225ee34b6e0fee19f6ed2d285765d48d99
Nick Mathews <nick@codemettle.com>
2015-03-03 08:40:53 -0500

    PIN screen!
    Added the PIN screen as the first activity in the app. It actually checks for a "valid" PIN (default 1234) before sending off to the main activity. Still need to add a way to customize the PIN and set it on the first run.

---
+++ app/src/main/java/com/boboddy/vault/PIN.java
@@ -0,0 +1,85 @@
+package com.boboddy.vault;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class PIN extends ActionBarActivity {
+
+    TextView title;
+    EditText pin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin);
+
+        title = (TextView) findViewById(R.id.unlock_title);
+        pin = (EditText) findViewById(R.id.pin_edittext);
+        pin.setGravity(Gravity.CENTER);
+        pin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                String savedPin = prefs.getString("pin", "1234");
+                String enteredPin = s.toString();
+
+                if(enteredPin.equals(savedPin)) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+
+                    //Done with the PIN
+                    finish();
+                } else {
+                    if(enteredPin.length() > 3) {
+                        title.setText(R.string.wrong_pin);
+                        title.setTextColor(Color.RED);
+                    }
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_pin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
